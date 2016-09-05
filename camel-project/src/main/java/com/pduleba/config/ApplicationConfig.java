aeb0bb9145dd942be0e32bc759899c116f1a6a6e
pduleba <pduleba@pgs-soft.com>
2016-09-05 08:16:10 +0200

    Clean up

--- camel-project/src/main/java/com/pduleba/config/ApplicationConfig.java
+++ camel-project/src/main/java/com/pduleba/config/ApplicationConfig.java
@@ -13,10 +13,8 @@ import org.apache.camel.component.gson.GsonDataFormat;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import org.apache.cxf.bus.spring.SpringBus;
-import org.apache.cxf.feature.Feature;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.provider.json.JSONProvider;
-import org.apache.cxf.jaxrs.swagger.Swagger2Feature;
 import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -57,27 +55,18 @@ public class ApplicationConfig extends CamelConfiguration {
 	public CompanyResource companyResource() {
 		return new CompanyResource();
 	}
-
-	@Bean
-	public Swagger2Feature swagger2feature() {
-		Swagger2Feature swagger2Feature = new Swagger2Feature();
-		
-		return swagger2Feature;
-	}
 	
 	@Bean(name = REST_BEAN_ID)
 	@DependsOn("cxf")
 	public JAXRSServerFactoryBean rsServer(CompanyResource companyResource,
 			@Qualifier(DATA_FORMAT_DEFAULT_CXF_PROVIDER_BEAN_ID) JSONProvider<Object> defaultProvider,
 			@Qualifier(DATA_FORMAT_JACKSON_CXF_PROVIDER_BEAN_ID) JacksonJsonProvider jacksonProvider,
-			Swagger2Feature swagger2feature,
 			JaxRsApiApplication jaxRsApiApplication, SpringBus cxf,
 			@Value("${use.jackson.provider}") boolean useJacksonProvider) {
 		JAXRSServerFactoryBean factory = new JAXRSServerFactoryBean();
 		
 		factory.setBus(cxf);
 		factory.setServiceBeans(Arrays.<Object>asList(companyResource));
-		factory.setFeatures(Arrays.<Feature>asList(swagger2feature));
 		factory.setAddress("http://localhost:9000/api");
 		if (useJacksonProvider) {
 			factory.setProviders(Arrays.<Object>asList(jacksonProvider));
